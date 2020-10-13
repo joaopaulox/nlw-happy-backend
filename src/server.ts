1a0e5adef25809cf8d806b0cec3a92263dabe539
jp <jptick@gmail.com>
2020-10-13 16:58:27 -0300

    Create routes and controller

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import './database/connection';
+
+import routes from './routes.ts';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
